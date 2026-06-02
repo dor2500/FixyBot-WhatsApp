efcb61abd169c06139b7992b09d47f037ab72a42
dor2500 <50528935+dor2500@users.noreply.github.com>
2026-06-02 22:05:02 +0300

    Optimize Puppeteer for 512MB RAM

--- whatsapp_bridge.js
+++ whatsapp_bridge.js
@@ -8,7 +8,16 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // This saves a lot of memory!
+            '--disable-gpu'
+        ]
     }
 });
 
